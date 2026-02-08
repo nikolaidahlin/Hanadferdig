9101284b8b2adf35eb55060c612f2a91ec86a87b
nikolaidahlin <nikolai.dahlin@hotmail.com>
2026-02-08 16:04:33 +0100

    fixed ":" on clock , and no longer updates ea sec

--- clock.js
+++ clock.js
@@ -4,10 +4,8 @@ function updateClock() {
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
     
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const timeString = `${hours}:${minutes} ${seconds}`;
     
     document.getElementById('clock').textContent = timeString;
 }
-
-setInterval(updateClock, 1000);
 updateClock(); 
